8c83a95c9c4a840d6032bd327c31c031c3b39a58
Ken Yu <kyu_115s@hotmail.com>
2016-10-05 23:25:05 +0800

    Adapt to LaS-VPE Platform.
    Add license and some documentation.
    
    Add test. Reset to old struct name to recover compatibility with given lib.
    
    Adapt to lower version of cmake.
    
    Add configuration for test. Add test without using OpenCV.
    
    Modify function test.
    
    Add test video. Repair sample configuration file.
    
    Add A Test Video
    
    no more
    
    lida
    
    Fix memory leak and adapt to newest version of LaS-VPE-Platform.

--- include/basic_define.h
+++ include/basic_define.h
@@ -1,8 +1,10 @@
-/**\file    basic_define.h
- * \brief   some basic definitions ...
- * \version 0.3.1   2016/09/20,   0.3.2   2016/09/21
+/**@file    basic_define.h
+ * @brief   Basic definitions for object tracking.
+ * @version 0.3.1   2016/09/20,   0.3.2   2016/09/21
  *          Mainly modify vector to array; and delete member of 'video_url'. 
- * \email   da.li@cripac.ia.ac.cn
+ * @email   da.li@cripac.ia.ac.cn
+ *
+ * Licensed under The MIT License [see LICENSE for details]
  */
 
 #ifndef _BASIC_DEFINE_H_
@@ -14,6 +16,12 @@
 
 #define VIDEO_URL_LEN 2048
 
+/**
+ * @struct BoundingBox.
+ * @brief Bounding box of a pedestrian.
+ * Each bounding box represents the location as well as the size
+ * of a pedestrian in a frame.
+ */
 typedef struct _bb_t {
   int x;   // Left
   int y;   // Top
@@ -22,18 +30,19 @@ typedef struct _bb_t {
   unsigned char *patch_data;
 } BoundingBox;
 
-// typedef vector<BoundingBox> BBList;
-
+/**
+ * @struct Trajectory
+ * @brief Trajectory of a target pedestrian.
+ * A tracklet represents a series of appears of a target pedestrian
+ * in a video.
+ */
 typedef struct _trajectory_t {
-  int id;              // It is only unique inside one video.
+  int id;
   int traj_size;
-  // string video_url;
   char video_url[VIDEO_URL_LEN];
   int start_frame_idx;
   // BBList location_sequence;
   BoundingBox *location_sequence;
 } Trajectory;
-
-// typedef vector<Track> TrackList;
 
 #endif // _BASIC_DEFINE_H_
